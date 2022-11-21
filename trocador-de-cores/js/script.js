adf664d250610f5fbac740c70cea956623e94e8a
Kelly Santos <98798428+Kellycns@users.noreply.github.com>
2022-11-21 12:38:34 -0300

    Update script.js

--- trocador-de-cores/js/script.js
+++ trocador-de-cores/js/script.js
@@ -2,21 +2,19 @@ const btnAzul = document.querySelector('#azul');
 const btnAmarelo = document.querySelector('#amarelo');
 const btnVerde = document.querySelector('#verde');
 const btnVermelho = document.querySelector('#vermelho');
-const bg = document.querySelector('body');
 
 btnAzul.addEventListener('click', () => {
-    bg.style.backgroundColor = 'rgb(16, 64, 176)';
-
+    document.body.style.backgroundColor = 'rgb(16, 64, 176)';
 });
 
 btnAmarelo.addEventListener('click', () => {
-    bg.style.backgroundColor = 'rgb(161, 171, 14)';
+    document.body.style.backgroundColor = 'rgb(161, 171, 14)';
 });
 
 btnVerde.addEventListener('click', () => {
-    bg.style.backgroundColor = 'rgb(60, 176, 60)';
+    document.body.style.backgroundColor = 'rgb(60, 176, 60)';
 });
 
 btnVermelho.addEventListener('click', () => {
-    bg.style.backgroundColor = 'rgb(201, 29, 29)';
+    document.body.style.backgroundColor = 'rgb(201, 29, 29)';
 });
